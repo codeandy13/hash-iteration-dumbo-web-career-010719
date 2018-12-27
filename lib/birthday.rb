e381bb325b5776ddb994212816cc48001356d3bc
Andrew Kim <akim618@gmail.com>
2018-12-27 23:28:10 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,8 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! Yor are now #{age} years old!"
 end
 
 
