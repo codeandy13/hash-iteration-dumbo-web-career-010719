4543efd523301a738d071aacb46a3bccef68c28b
Andrew Kim <akim618@gmail.com>
2018-12-27 23:34:46 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,14 +4,14 @@
 #   "Amanda" => 27
 # }
 
-# def happy_birthday(birthday_kids)
-#   birthday_kids.each do |kids_name, age|
-#     puts "Happy Birthday #{kids_name}! Yor are now #{age} years old!"
-# end
-#
-# end
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
+    puts "Happy Birthday #{kids_name}! Yor are now #{age} years old!"
 end
+
+end
+# def happy_birthday(birthday_kids)
+#   birthday_kids.each do |kids_name, age|
+#     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+#   end
+# end
